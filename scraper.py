365195e5f1930ed3c1fea2270cc876ec709a3358
Alex Day <alexanderday@email.arizona.edu>
2021-10-20 03:20:45 -0700

    Create base web scraping object

--- scraper.py
+++ scraper.py
@@ -1,19 +1,29 @@
 from requests_html import HTMLSession
 from bs4 import BeautifulSoup
 
-s = HTMLSession()
-URL = 'https://www.amazon.com/House-Suns-Alastair-Reynolds/dp/0316462624/ref=tmm_pap_swatch_0?_encoding=UTF8&coliid=I8NBYXQ5B55M9&colid=1ZGJ9HISC21A9&qid=&sr=&pldnSite=1'
-header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36', 
-'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 
-'Accept-Language' : 'en-US,en;q=0.5',
-'Accept-Encoding' : 'gzip', 
-'DNT' : '1', # Do Not Track Request Header 
-'Connection' : 'close'}
+class Scraper(object):
+    '''
+    Main web scraping object. Takes in a URL and requested data point such as 
+    price or title of the object and returns a string of that data.
+    '''
 
-r = s.get(URL, headers=header)
+    def __init__(self):
+        s = HTMLSession()
+        URL = 'https://smile.amazon.com/dp/0593298683'
+        header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36', 
+        'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 
+        'Accept-Language' : 'en-US,en;q=0.5',
+        'Accept-Encoding' : 'gzip', 
+        'DNT' : '1',
+        'Connection' : 'close'}
 
-soup = BeautifulSoup(r.text, "html.parser")
+        r = s.get(URL, headers=header)
 
-results = soup.find(id='price')
+        soup = BeautifulSoup(r.text, "html.parser")
 
-print(results)
+        results = soup.find(id='price')
+
+        print(results.get_text())
+
+if __name__ == '__main__':
+    scraper = Scraper()
