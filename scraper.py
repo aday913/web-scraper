8a6e2f4ab88ab179c3b561461dd17ad087634cd0
aday913 <45890246+aday913@users.noreply.github.com>
2021-10-20 17:57:24 -0700

    Incorporate emailer object into scraper

--- scraper.py
+++ scraper.py
@@ -1,6 +1,8 @@
 from requests_html import HTMLSession
 from bs4 import BeautifulSoup
 
+from mailer import Emailer
+
 class Scraper(object):
     '''
     Main web scraping object. Takes in a URL and requested data point such as 
@@ -8,6 +10,8 @@ class Scraper(object):
     '''
 
     def __init__(self):
+
+        test = Emailer()
         s = HTMLSession()
         URL = 'https://smile.amazon.com/dp/0593298683'
         header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36', 
@@ -24,6 +28,14 @@ class Scraper(object):
         results = soup.find(id='price')
 
         print(results.get_text())
+
+        print('Trying to send an email...')
+        test.sendMessage(
+            '''\
+            Hey there! This is a test email, where the price for the item is {}!
+            '''.format(results.get_text())
+            )
+        print('Email sent.')
 
 if __name__ == '__main__':
     scraper = Scraper()
