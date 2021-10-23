a43bf25155d359b27ce198d47a2941128e3765b0
aday913 <45890246+aday913@users.noreply.github.com>
2021-10-22 17:54:22 -0700

    Create data manager object

--- datamanager.py
+++ datamanager.py
@@ -1,28 +1,53 @@
 import sqlite3
+import pandas as pd
 
-# Create a connection to the local database
-connection = sqlite3.connect("myFirstTable.db")
+class DataManager(object):
+    '''
+    Main SQL-facing object that allows the user/bot to grab, insert, or alter 
+    data in the database associated with web scraping.
+    '''
+    def __init__(self):
+        # Create a connection to the local database
+        connection = sqlite3.connect("scrapingData.db")
 
-# Begin a query
-cursor = connection.cursor()
+        # Begin a query
+        cursor = connection.cursor()
 
-# Create our command we want the database to process
-sql_command = """CREATE TABLE test (  
-person_number INTEGER PRIMARY KEY,  
-fname VARCHAR(20),  
-lname VARCHAR(30),  
-gender CHAR(1),  
-joining DATE);"""
+        # Create our command we want the database to process
+        sql_command = """CREATE TABLE people (  
+        person_number INTEGER PRIMARY KEY,  
+        fname VARCHAR(20),  
+        lname VARCHAR(30),  
+        gender CHAR(1),  
+        joining DATE);"""
 
-# Execute the command that we just made
-cursor.execute(sql_command)
+        # Execute the command that we just made
+        cursor.execute(sql_command)
 
-# Add a row to the table we just made
-sql_command = """INSERT INTO people VALUES (1, "Alex", "Day", "M", "2020-11-15");"""
-cursor.execute(sql_command)
+        # Add a row to the table we just made
+        sql_command = """
+        INSERT INTO people VALUES (2, "Alex", "Day", "M", "2020-11-15");
+        """
+        cursor.execute(sql_command)
 
-# Commit the commands we just executed
-connection.commit()
+        # Commit the commands we just executed
+        connection.commit()
 
-# Close our connection
-connection.close()
+        print('Trying to get data...')
+
+        com = """
+        SELECT * FROM people;
+        """
+        cursor = connection.cursor()
+        cursor.execute(com)
+        ans = cursor.fetchall()
+        print(ans)
+
+        df = pd.read_sql_query("SELECT * FROM people;", connection)
+        print(df.head())
+
+        # Close our connection
+        connection.close()
+
+if __name__ == "__main__":
+    manager = DataManager()
